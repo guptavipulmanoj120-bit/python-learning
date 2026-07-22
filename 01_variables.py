f24f04c6f429bec4c818acf7878513364e332ba2
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-22 20:08:16 +0530

    Add variables and data types examples
    Added examples of Python variables, data types, type checking, and multiple variable assignment.

---
+++ 01_variables.py
@@ -0,0 +1,23 @@
+# Integer variable
+x = 10
+print(type(x))
+
+# Different data types
+name = "Vipul"
+age = 28
+height = 5.9
+is_student = True
+
+print(name, age, height, is_student)
+
+# Checking data types
+print(type("Hello"))   # str
+print(type(12.5))      # float
+print(type(True))      # bool
+
+# Multiple variable assignment
+a, b, c = 10, 20, 30
+
+print(a)
+print(b)
+print(c)
