73301a5a9e451e9766bf1048b0853278e02bf01d
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-23 20:46:52 +0530

    Add user input examples
    Added examples demonstrating how to take user input using the input() function and use the entered values in Python programs.

---
+++ 04_input.py
@@ -0,0 +1,19 @@
+num1= int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+print("Addition: ",num1 + num2)
+print("Subraction: ",num1 - num2)
+print("Multiplication: ",num1 * num2)
+print("Division: ",num1 / num2)
+print("Floor Division: ",num1 // num2)
+print("Modules: ",num1 % num2)
+print("Power: ",num1 ** num2)
+
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+print("Area of rectangle is: ",length * width)
+
+radius = int(input("Enter radius: "))
+Area = 3.14 * radius * radius
+print("Area of circle is: ",Area)
+
