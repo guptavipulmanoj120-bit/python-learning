1a3655ba1a3a612b75c65d1051db347728489dd8
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-24 21:09:44 +0530

    Add basic Python calculator
    Built a basic calculator using Python variables, user input, and arithmetic operators. This project is part of my Python learning journey.

---
+++ mini calculator.py
@@ -0,0 +1,23 @@
+print("=" * 30)
+print("    PYTHON CALCULATOR")
+print("=" * 30)
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+power = num1 ** num2
+
+print("\nResults")
+print("Addition       :", addition)
+print("Subtraction    :", subtraction)
+print("Multiplication :", multiplication)
+print("Division       :", division)
+print("Floor Division :", floor_division)
+print("Modulus        :", modulus)
+print("Power          :", power)
