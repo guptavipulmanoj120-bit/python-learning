996609e4d8635813534bdd208dcb1a366f195d35
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-24 20:29:36 +0530

    Add variables, input, and operators
    Python practice repository covering variables, data types, user input, operators, and more as I learn Python from the basics.

---
+++ 01_variables_input_operators.py
@@ -0,0 +1,26 @@
+name = "vipul"
+age = 21
+city = "mumbai"
+print(name , age , city)
+
+# favorite movie = "spider-men"
+# favorite programming langauge = "python"
+# favorite number = 24
+# print(favorite movie,favorite programming langauge,favorite number)
+
+
+
+print(type("vipul"))
+print(type(21))
+print(type(25.5))
+print(type(True))
+
+input("hello: ")
+input("you are years old: ")
+input("you live in: ")
+print("hello")
+print("you are yeara old")
+print("you live in ")
+
+
+
