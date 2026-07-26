45789afa7c641bd5e4b6d9fbd75bc1a8909f0b7a
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-26 19:33:39 +0530

    Add string indexing and slicing examples
    Added beginner examples demonstrating Python string indexing, positive and negative indexing, and string slicing with different start and end positions.

---
+++ 07_string_indexing_slicing.py
@@ -0,0 +1,10 @@
+name = "vipul"
+
+print(name[0:3])
+
+print(name[-4: -1]) #if the number is negative to solve this convert into postive 
+print(name[1:4])    # both output come same 
+
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:5])
+print(name[1:5])
