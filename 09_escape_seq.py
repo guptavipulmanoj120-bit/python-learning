ea81ab290f4a7926f7daca78590b4eed8942c9ad
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-27 22:32:24 +0530

    add escape sequence
    add new escape sequence

---
+++ 09_escape_seq.py
@@ -0,0 +1,36 @@
+#\n → New Line
+print("hello\nworld")
+
+#\t → Tab Space
+print("vipul\tsaloni")
+
+#\\ → Print a Backslash
+print("vipul\\saloni")
+
+#\' → Single Quote
+print("vipul\'gupta")
+
+#\" → Double Quote
+print("he said\"hello\"")
+
+#\b → Backspace
+print("Helloo\b")
+
+#\r → Carriage Return
+print("hello\rgame")
+
+#\a → Alert (Bell)
+print("\a")
+
+#\f → Form Feed
+print("hello\fworld")
+
+#\v → Vertical Tab
+print("hello\vworld")
+
+#Raw Strings (r"")
+path = r"C:\Users\Vipul\Documents"
+
+print(path)
+
+
