b974eee185a244c67fde51ecdfae8249c857d358
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-27 22:34:35 +0530

    Add string indexing and slicing examples
    Learned Python string indexing and slicing.
    
    Topics covered:
    - Positive indexing
    - Negative indexing
    - String slicing
    - Default slice values
    - Accessing first and last characters
    - Reversing strings using slicing

---
+++ 02_string_practice.py
@@ -0,0 +1,24 @@
+# Reverse a string
+name = input("Enter your name: ")
+print(name[::-1])
+
+# First three characters
+word = input("Enter your word: ")
+print(word[0:3])
+
+# Last three characters
+word = input("Enter your word: ")
+print(word[-3:])
+
+# First and last character
+name = input("Enter your name: ")
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+# Greeting
+name = input("Enter your name: ")
+print("Hello {}.".format(name))
+
+# Length of a sentence
+sentence = input("Enter your sentence: ")
+print(len(sentence))
