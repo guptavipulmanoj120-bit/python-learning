452085e4c7c067e2647c5e1750e668efcca451f9
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-07-31 22:18:09 +0530

    list methods added
    all methods of list

---
+++ 12_List_methods.py
@@ -0,0 +1,26 @@
+# List methods
+
+# 1 append() -> Add an iteam to the end
+
+fruits1 = ["Apple", "Banana"]
+
+fruits1.append("Mango")
+
+print(fruits1)
+
+# 2 insert() -> Add at a specific position
+
+fruits2 = ["Apple", "Banana", "Orange"]
+
+fruits2.insert(1,"Mango")
+
+print(fruits2) 
+
+# 3 remove() -> Remove by value
+
+fruits3 = ["Apple", "Banana", "Mango"]
+
+fruits3.remove("Banana")
+
+print(fruits3)
+
