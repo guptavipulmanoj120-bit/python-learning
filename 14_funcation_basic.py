cb81cf58b827436439836c4da20f5b478d325a8e
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-08-15 21:15:34 +0530

    Add multiple function examples in 14_funcation_basic.py

--- 14_funcation_basic.py
+++ 14_funcation_basic.py
@@ -1,1 +1,72 @@
-14_funcation_basic
+# Example 1: Simple Function
+
+def hello():
+    print("Hello World")
+
+hello()
+
+
+# Example 2: Function to Print Name
+
+def name():
+    print("Vipul")
+
+name()
+
+
+# Example 3: Motivation Function
+
+
+def motivation():
+    print("I don't give up until I win.")
+
+motivation()
+
+
+# Example 4: Function with One Parameter
+
+
+def country(name):
+    print("I love", name)
+
+country("India")
+
+
+# Example 5: Function with One Parameter
+
+
+def age(number):
+    print("Your age is", number)
+
+age(20)
+
+
+# Example 6: Function with Parameter
+
+
+def motivation_line(line):
+    print(line)
+
+motivation_line("I don't give up until I win.")
+
+
+# Example 7: Function with Two Parameters
+
+
+def add(a, b):
+    print(a + b)
+
+add(5, 3)
+add(10, 20)
+add(1, 9)
+
+
+# Example 8: Multiplication Function
+
+
+def multiply(a, b):
+    print(a * b)
+
+multiply(2, 6)
+multiply(7, 5)
+multiply(10, 10)
