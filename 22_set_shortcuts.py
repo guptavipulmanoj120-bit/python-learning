83c3015a7cff5d51b133d9d46288386cdc8e2621
Vipul Gupta <guptavipulmanoj120@gmail.com>
2026-08-26 21:40:34 +0530

    set shortcuts
    set shortcuts

---
+++ 22_set_shortcuts.py
@@ -0,0 +1,16 @@
+a.union(b)        # everything
+a | b             # same thing
+
+
+a.intersection(b) # common
+a & b             # same thing
+
+
+a.difference(b)   # a minus b
+a - b             # same thing
+
+
+a.symmetric_difference(b) # Elements in either a or b, but not both
+a ^ b                     # same thing
+
+
